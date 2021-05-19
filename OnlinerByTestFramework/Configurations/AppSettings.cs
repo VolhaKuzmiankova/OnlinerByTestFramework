60a0b0fda0f308da517eef3364f2d4cda25ae721
olgayuks <yuks230990@gmail.com>
2021-05-19 22:56:39 +0300

    create driver factory

--- OnlinerByTestFramework/Configurations/AppSettings.cs
+++ OnlinerByTestFramework/Configurations/AppSettings.cs
@@ -3,5 +3,7 @@
     public class AppSettings
     {
         public ServiceSettings Services { get; set; }
+        
+        public Configuration Configuration { get; set; }
     }
 }
---
+++ OnlinerByTestFramework/Factories/DriverFactory.cs
@@ -0,0 +1,23 @@
+﻿using OnlinerByTestFramework.Enums;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace OnlinerByTestFramework.Factories
+{
+    public static class DriverFactory
+    {
+        public static IWebDriver GetDriver(BrowserName name)
+        {
+            switch (name)
+            {
+                case BrowserName.Chrome:
+                    return new ChromeDriver();
+                case BrowserName.FireFox:
+                    return new FirefoxDriver();
+                default:
+                    return default;
+            }
+        }
+    }
+}
